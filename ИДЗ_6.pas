1a77c4794971b6b26c73283875bf493387aaf6f3
yumuii <yu.balganova@yandex.ru>
2025-03-21 18:23:43 +1000

    CREATE: описала данные

---
+++ ИДЗ_6.pas
@@ -0,0 +1,21 @@
+﻿program LAB_6_Records;
+
+type
+  shop = record
+    name: string;
+  end;
+
+
+type
+  product = record
+    name: string;
+    shops: array of shop;
+  end;
+
+var
+  products: array of product;
+
+
+begin
+  
+end.

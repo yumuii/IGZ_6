093e1ea2e24fe28fbff325835c7d22caf4b7817e
yumuii <yu.balganova@yandex.ru>
2025-03-25 09:44:42 +1000

    CREATE: сделала процедуру чтания и записывания файлов

--- ИДЗ_6.pas
+++ ИДЗ_6.pas
@@ -2,39 +2,39 @@
 
 
 type
-  shop = record
-    name: string;
-  end;
+    shop = record
+        name: string;
+    end;
 
 
 type
-  product = record
-    name: string;
-    shops: array of shop;
-  end;
+    product = record
+        name: string;
+        shops: array of shop;
+    end;
 
 var
-  shops: array of string;
-  products: array of string;
+    shops: array of string;
+    products: array of string;
 
 
 procedure read_file(file_name: string; var arr: array of string);
 var
-  f: text;
-  line: string;
-  count: integer;
+    f: text;
+    line: string;
+    count: integer;
 begin
-  count := 0;
-  assign(f, file_name);
-  reset(f);
-  while not Eof(f) do
-  begin
-    readln(f, line);
-    count := count + 1;
-    setlength(arr, count);
-    arr[count - 1] := line;
-  end;
-  close(f); 
+    count := 0;
+    assign(f, file_name);
+    reset(f);
+    while not Eof(f) do
+    begin
+        readln(f, line);
+        count := count + 1;
+        setlength(arr, count);
+        arr[count - 1] := line;
+    end;
+    close(f); 
 end;
 
 
@@ -53,11 +53,11 @@ end;
 
 
 begin
-  
-  read_file('shops.txt', shops);
-  read_file('products.txt', products);
-
-  writeln(shops);
-  writeln(products);
-  
+    
+    read_file('shops.txt', shops);
+    read_file('products.txt', products);
+    
+    writeln(shops);
+    writeln(products);
+    
 end.
